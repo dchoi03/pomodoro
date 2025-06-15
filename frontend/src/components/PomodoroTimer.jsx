56f2daa2b0c51006d4922ec044e6a9002df80df0
David <dchoi2003@gmail.com>
2025-06-15 18:02:12 +1000

    Added long break interval when pomodoro timer reaches 4 study intervals

--- frontend/src/components/PomodoroTimer.jsx
+++ frontend/src/components/PomodoroTimer.jsx
@@ -9,14 +9,44 @@ function PomodoroTimer() {
   const [studyIntervals, setStudyIntervals] = useState(0)
 
   useEffect(() => {
-    if (!isRunning) return;
+    if (!isRunning || time <= 0) return;
 
     const interval = setInterval(() => {
-      setTime((prevTime) => prevTime - 1000);
+        setTime((prevTime) => prevTime - 1000)
     }, 1000);
 
     return () => clearInterval(interval);
   }, [isRunning]);
+
+  useEffect(()=> {
+    if (time > 0) return;
+
+    setIsRunning(false)
+
+    if (!isBreak) {
+      setIsBreak(true);
+      setStudyIntervals((prev) => prev + 1);
+    } else {
+      setIsBreak(false)
+    }
+
+  },[time])
+
+  useEffect(() => {
+    if (isBreak) {
+      if (studyIntervals === 4) {
+        setTime(1800000); // long break
+        setStudyIntervals(0);
+      } else {
+        setTime(300000); // short break
+      }
+      setIsRunning(true); // start the break
+    } else if (time === 0 && !isBreak) {
+      setTime(1500000); // next focus session
+      setIsRunning(true);
+    }
+  }, [isBreak]);
+
 
 
   const formattedTime = (milliseconds) => {
@@ -26,11 +56,18 @@ function PomodoroTimer() {
     let seconds = parseInt(total_secs % 60)
     let minutes = parseInt(total_minutes % 60)
 
-    return `${minutes}:${seconds}`
+    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
   }
 
   const handleTimer = () => {
     setIsRunning((prev) => !prev)
+  }
+
+  const resetTimer = () => {
+    setIsRunning(false)
+    setIsBreak(false)
+    setTime(1500000)
+    setStudyIntervals(0)
   }
 
   return (
@@ -44,6 +81,14 @@ function PomodoroTimer() {
           (isRunning ? 'Pause' : 'Resume')
           }
         </Button>
+        {
+          (time != 1500000) ?
+          <Button onClick={resetTimer}>Reset</Button>
+          : null
+        }
+      </div>
+      <div className='text-white'>
+        {studyIntervals}
       </div>
     </div>
   )
